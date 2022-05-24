735f175a6da6bc1a4e36fcdddfb20b35bb4d958e
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-24 16:05:13 +0200

    aggiornamento

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+// (va bene qualsiasi immagine che prendete dal web e/o avete e/o vi scaricate, è indifferente)
 
 const root = new Vue({
     el : "#container",
 
     data : {
-        stringa : "ciao",
+        stringa : "Vue Hello",
+
+        immagine: "https://picsum.photos/200/300",
     }
 })
